c3d64497185e7b9fc555f229881e265ff677e116
lkathula <lkathula@asu.edu>
2022-10-27 22:55:13 -0700

    two functions tested

--- src/urinals.java
+++ src/urinals.java
@@ -16,14 +16,57 @@ public class urinals {
     }
     public static int countUrinals(char[] inp)
     {
-        return 0;
+        int free=0;
+        for(int i=0;i<inp.length;i++)
+        {
+            if(i==0)
+            {
+                if(inp[1]=='1')
+                {
+                    continue;
+                }
+                else
+                {
+                    if(inp[i]=='0')
+                    {
+                        inp[i]='1';
+                        free++;
+                    }
+
+                }
+            }
+            else if(i==inp.length-1)
+            {
+                if(inp[inp.length-2]=='1')
+                {
+                    continue;
+                }
+                else
+                {
+                    if(inp[i]=='0')
+                    {
+                        inp[i]='1';
+                        free++;
+                    }
+                }
+            }
+            else
+            {
+                if(inp[i-1]=='0'&&inp[i+1]=='0'&&inp[i]=='0')
+                {
+                    inp[i]='1';
+                    free++;
+                }
+
+            }
+        }
+        return free;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner inFile=new Scanner(new FileReader("urinal.dat"));
         String finp=inFile.useDelimiter("\\A").next(); //For reading input from file and converting the content into a string
-        char[] c={'0','0','0','0'};
-        System.out.println(goodString(c));
+
 
 
     }
--- src/urinalsTest.java
+++ src/urinalsTest.java
@@ -10,14 +10,19 @@ class urinalsTest {
         String s1="011";
         String s2="00010";
         assertAll(() -> assertEquals(false, urinals.goodString(s1.toCharArray())),
-                () -> assertEquals(false,urinals.goodString(s2.toCharArray())));
+                () -> assertEquals(true,urinals.goodString(s2.toCharArray())));
     }
 
     @org.junit.jupiter.api.Test
     void countUrinals() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void main() {
+        System.out.println("Lalith Kumar Kathula - 4 Tests  Implemented");
+        String s1="010";
+        String s2="00010";
+        String s3="00000";
+        String s4="0000";
+        assertAll(() -> assertEquals(0, urinals.countUrinals(s1.toCharArray())),
+                () -> assertEquals(1,urinals.countUrinals(s2.toCharArray())),
+                    () -> assertEquals(3,urinals.countUrinals(s3.toCharArray())),
+        () -> assertEquals(2,urinals.countUrinals(s4.toCharArray())));
     }
 }
