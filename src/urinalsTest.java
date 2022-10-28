37777a5c2c3345041cd694ed05bc110f95e4abdb
lkathula <lkathula@asu.edu>
2022-10-27 21:57:18 -0700

    skeleton

---
+++ src/urinalsTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class urinalsTest {
+
+    @org.junit.jupiter.api.Test
+    void goodString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void countUrinals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
